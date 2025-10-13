d626a4d054a7658f211b1b224a0d9f9944dca388
SneiderGL <hsgrajalesl@udistrital.edu.co>
2025-10-12 20:23:04 -0500

    Primer commit: estructura base del proyecto y prueba de ejecución

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Parcial Iniciado");
+        System.out.println("Todo funcionando correctamente :)");
+    }
+}
