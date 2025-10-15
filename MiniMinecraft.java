4f3432588f991ea181211511f5097da7b69647f5
SneiderGL <hsgrajalesl@udistrital.edu.co>
2025-10-14 23:45:38 -0500

    se mejoro el code del juego libre

--- MiniMinecraft.java
+++ MiniMinecraft.java
@@ -14,9 +14,9 @@ public class MiniMinecraft {
             System.out.println("1. Comer");
             System.out.println("2. Minar");
             System.out.println("3. Pelear");
-            System.out.println("4. Vencer al dragón");
+            System.out.println("4. Vencer al dragon");
             System.out.println("5. Salir del juego");
-            System.out.print("Elige una opción: ");
+            System.out.print("Elige una opcion: ");
             int opcion = leer.nextInt();
             leer.nextLine();
 
@@ -32,16 +32,16 @@ public class MiniMinecraft {
                     break;
                 case 4:
                     if (xp >= 100) {
-                        System.out.println("\n¡Te enfrentas al dragón!");
-                        if (rand.nextInt(100) < 80) {
-                            System.out.println("¡Ganaste! Has vencido al dragón 🐉");
+                        System.out.println("\nTe enfrentas al dragon...");
+                        if (rand.nextInt(100) < 65) {
+                            System.out.println("Has vencido al dragon. Eres el heroe de Minecraft!");
                             jugando = false;
                         } else {
-                            System.out.println("¡Perdiste la pelea! Pierdes 35 XP");
-                            xp -= 35;
+                            System.out.println("El dragon te derroto. Pierdes 40 XP.");
+                            xp -= 40;
                         }
                     } else {
-                        System.out.println("No puedes pelear con el dragón, necesitas 100 XP.");
+                        System.out.println("No tienes suficiente XP. Necesitas 100 XP para pelear con el dragon.");
                     }
                     break;
                 case 5:
@@ -49,58 +49,60 @@ public class MiniMinecraft {
                     jugando = false;
                     break;
                 default:
-                    System.out.println("Opción no válida.");
+                    System.out.println("Opcion no valida.");
             }
         }
         System.out.println("XP final: " + xp);
     }
 
+    // --- Comer ---
     private static int comer(Random rand) {
         int prob = rand.nextInt(100);
-        if (prob < 20) {
-            System.out.println("Comiste carne. +5 XP");
-            return 5;
-        } else if (prob < 70) {
+        if (prob < 30) { // carne
+            System.out.println("Comiste carne. +6 XP");
+            return 6;
+        } else if (prob < 80) { // zanahorias
             System.out.println("Comiste zanahorias. +3 XP");
             return 3;
-        } else {
-            System.out.println("Te dio hambre. -3 XP");
-            return -3;
+        } else { // hambre
+            System.out.println("Te dio hambre. -5 XP");
+            return -5;
         }
     }
 
+    // --- Minar ---
     private static int minar(Random rand) {
         int prob = rand.nextInt(100);
-        if (prob < 40) {
-            System.out.println("Encontraste carbón. +0 XP");
-            return 0;
-        } else if (prob < 60) {
-            System.out.println("Encontraste hierro. +5 XP");
-            return 5;
-        } else if (prob < 70) {
-            System.out.println("Encontraste diamante. +20 XP");
-            return 20;
-        } else {
-            System.out.println("Caiste en lava. -15 XP");
-            return -15;
+        if (prob < 40) { // carbon
+            System.out.println("Encontraste carbon. +1 XP");
+            return 1;
+        } else if (prob < 75) { // hierro
+            System.out.println("Encontraste hierro. +6 XP");
+            return 6;
+        } else if (prob < 90) { // diamante
+            System.out.println("Encontraste diamante. +15 XP");
+            return 15;
+        } else { // lava
+            System.out.println("Caíste en lava. -12 XP");
+            return -12;
         }
     }
 
+    // --- Pelear ---
     private static int pelear(Random rand) {
         int prob = rand.nextInt(100);
-        if (prob < 20) {
-            System.out.println("Te explotó un creeper. -7 XP");
-            return -7;
-        } else if (prob < 30) {
-            System.out.println("Ganaste a un creeper. +10 XP");
-            return 10;
-        } else if (prob < 80) {
-            System.out.println("Te pegó un zombie. -2 XP");
-            return -2;
-        } else {
-            System.out.println("Le ganaste a un zombie. +7 XP");
-            return 7;
+        if (prob < 15) { // creeper explota
+            System.out.println("Te exploto un creeper. -10 XP");
+            return -10;
+        } else if (prob < 25) { // ganaste creeper
+            System.out.println("Derrotaste a un creeper. +12 XP");
+            return 12;
+        } else if (prob < 65) { // zombie te pega
+            System.out.println("Te golpeo un zombie. -4 XP");
+            return -4;
+        } else { // ganaste zombie
+            System.out.println("Derrotaste a un zombie. +8 XP");
+            return 8;
         }
     }
 }
-
